eb861c26da9da641366f7bf3d7ada3e01f5869dc
Advait <126783821+Advaitgaur004@users.noreply.github.com>
2024-05-28 20:49:57 +0530

    chore: Update admin.py and serializers.py in student and TA modules, and settings.py

--- server/TA/admin.py
+++ server/TA/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import InstructorProfile, StudentTaskStatus
 
-admin.site.register(InstructorProfile)
-admin.site.register(StudentTaskStatus)
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    search_fields = ('user',)
+    ordering = ('user',)
+
+class StudentTaskStatusAdmin(admin.ModelAdmin):
+    list_display = ('student', 'task', 'completed', 'earned_xp')
+    search_fields = ('student', 'task')
+    ordering = ('student', 'task')
+
+admin.site.register(InstructorProfile, InstructorAdmin)
+admin.site.register(StudentTaskStatus, StudentTaskStatusAdmin)
